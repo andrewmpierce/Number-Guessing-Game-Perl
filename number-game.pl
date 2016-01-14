6597a7551052bbd3653cbe3605a4be0218e1d32c
apierce1992 <apierce1992@gmail.com>
2016-01-14 13:39:11 -0500

    basic number game setup

---
+++ number-game.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $range = 100;
+
+my $random_number = int(rand($range));
+print "Let's play a game! Pick a number between 1-100. You have 5 guesses.";
+
+while
+
+
+print "$random_number";
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is a simple number guessing game to make myself more familiar with basic Perl concepts.
+It is written in Perl 5 and can be played by running perl number-game.pl in your terminal.
