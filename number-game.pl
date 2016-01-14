621acfffba4da7e26ab6f72f7a6485ed2872f026
apierce1992 <apierce1992@gmail.com>
2016-01-14 13:47:26 -0500

    able to take user input

--- number-game.pl
+++ number-game.pl
@@ -5,9 +5,14 @@ use warnings;
 my $range = 100;
 
 my $random_number = int(rand($range));
-print "Let's play a game! Pick a number between 1-100. You have 5 guesses.";
+print "Let's play a game! Pick a number between 1-100. You have 5 guesses.\n";
+my $guessed_number = <STDIN>;
 
-while
+while ($guessed_number != $random_number) {
+  print "$random_number $guessed_number";
 
 
-print "$random_number";
+}
+
+
+print "$random_number $guessed_number";
