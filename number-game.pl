50d312fccd1cd8febe5af7ec46e1ed78591487d3
apierce1992 <apierce1992@gmail.com>
2016-01-14 14:11:40 -0500

    basic game functional

--- number-game.pl
+++ number-game.pl
@@ -8,18 +8,18 @@ my $random_number = int(rand($range));
 print "Let's play a game! Pick a number between 1-100. You have 5 guesses.\n";
 our $guessed_number = <STDIN>;
 
-while ($guessed_number != $random_number) {
+for (my $i =1; $i <= 4; $i++) {
   print "$random_number $guessed_number";
   if ($guessed_number > $random_number) {
-    print "Your guess was too high! Try again.\n";
+    print "Your guess was too high! You have guessed $i times. Try again.\n";
     our $guessed_number = <STDIN>;
   }
   elsif ($guessed_number < $random_number) {
-    print "Your guess was too low! Try again. \n";
+    print "Your guess was too low! You have guessed $i times. Try again. \n";
     our $guessed_number = <STDIN>;
   }
+  else {
+    print "You guessed the number! The answer was $random_number\n";
+    last;
+  }
 }
-
-print "You guessed the number! The answer was $random_number\n";
-
-print "$random_number $guessed_number";
