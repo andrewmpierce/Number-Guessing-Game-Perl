3f26afe27dbdf8457ba99d920fc580bda261c424
apierce1992 <apierce1992@gmail.com>
2016-01-14 14:02:23 -0500

    can evaluate guesses too high and too low

--- number-game.pl
+++ number-game.pl
@@ -6,18 +6,20 @@ my $range = 100;
 
 my $random_number = int(rand($range));
 print "Let's play a game! Pick a number between 1-100. You have 5 guesses.\n";
-my $guessed_number = <STDIN>;
+our $guessed_number = <STDIN>;
 
 while ($guessed_number != $random_number) {
   print "$random_number $guessed_number";
   if ($guessed_number > $random_number) {
-    print "Your guess was too high! Try again.";
-    my $guessed_number = <STDIN>;
-
+    print "Your guess was too high! Try again.\n";
+    our $guessed_number = <STDIN>;
   }
-
-
+  elsif ($guessed_number < $random_number) {
+    print "Your guess was too low! Try again. \n";
+    our $guessed_number = <STDIN>;
+  }
 }
 
+print "You guessed the number! The answer was $random_number\n";
 
 print "$random_number $guessed_number";
